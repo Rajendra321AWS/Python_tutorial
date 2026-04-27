1752d4fce53e3389e0cea1506f425c71809089c3
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-04-27 19:18:19 -0400

    added int and float variables

--- variables.py
+++ variables.py
@@ -3,6 +3,25 @@ NAME = "raj"
 AGE = "22"
 HEIGHT = 6
 
+# integerss
+a = 10
+b = 3
+
+# floa values
+x = 5.2
+y = 3.5
+
+# operation
+sum_int = a+b
+division = a / b
+sum_float = x+y
+
+
+
 print("Name", NAME)
 print("Age", AGE)
 print("height", HEIGHT)
+
+print("integer sum:", sum_int)
+print("Division (int -> float):", division)
+print("Float sum:", sum_float)
