cae3fa55d1e36ee6ed4421add8dddc3fa28d3865
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-04-27 19:42:57 -0400

    added int_var_float

--- variables.py
+++ variables.py
@@ -1,26 +1,28 @@
-""" Baisc python program with variables """
-NAME = "raj"
-AGE = "22"
-HEIGHT = 6
+""" Basic python program with variables """
 
-# integerss
-a = 10
-b = 3
+def main():
+    NAME = "raj"
+    AGE = 22
+    HEIGHT = 6
+# integers
+    a = 10
+    b = 3
+# flot values
+    x = 5.2
+    y = 3.5
 
-# floa values
-x = 5.2
-y = 3.5
+    sum_int = a + b
+    division = a / b
+    sum_float = x + y
 
-# operation
-sum_int = a+b
-division = a / b
-sum_float = x+y
+    print("Name:", NAME)
+    print("Age:", AGE)
+    print("Height:", HEIGHT)
+    print("Integer sum:", sum_int)
+    print("Division:", division)
+    print("Float sum:", sum_float)
 
 
-
-print("Name", NAME)
-print("Age", AGE)
-print("height", HEIGHT)
-print("integer sum:", sum_int)
-print("Division (int -> float):", division)
-print("Float sum:", sum_float)
+if __name__ == "__main__":
+    main()
+    
