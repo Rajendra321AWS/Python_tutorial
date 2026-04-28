521d5152c720714b9dd0e76d7d9ee44722025fdb
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-04-28 04:43:12 -0400

    Basic program01

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+""" Basic Math python program """
+def main():
+
+    # integers
+    a = 10
+    b = 5
+
+    addition = a + b
+    subtraction = a - b
+    multiplecation = a * b
+    division = a / b
+    
+    print ("Number A:", a)
+    print ("number b:", b)
+    print ("addition:", addition)
+    print ("subtraction:", subtraction)
+    print ("Multiplication", multiplecation)
+    print ("Division", division)
+
+if __name__ =="__main__": 
+    main()
