26b9dcb9837a9ccc40058b5b47a79bac05f967b6
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-04-28 05:18:03 -0400

    basic math program 01

--- basic_math.py
+++ basic_math.py
@@ -1,5 +1,7 @@
-""" Basic Math python program """
+"""Basic Math python program with addition multiplication"""
+
 def main():
+    """Basic arithmetic program"""
 
     # integers
     a = 10
@@ -7,15 +9,15 @@ def main():
 
     addition = a + b
     subtraction = a - b
-    multiplecation = a * b
+    multiplication = a * b
     division = a / b
     
-    print ("Number A:", a)
-    print ("number b:", b)
-    print ("addition:", addition)
-    print ("subtraction:", subtraction)
-    print ("Multiplication", multiplecation)
-    print ("Division", division)
-    
+    print("Number A:", a)
+    print("Number B:", b)
+    print("Addition:", addition)
+    print("Subtraction:", subtraction)
+    print("Multiplication:", multiplication)
+    print("Division:", division)
+
 if __name__ =="__main__": 
     main()
