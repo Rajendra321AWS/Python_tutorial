065bf9ba01a67ab461f706425aa978a6aa9348e4
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-04-29 05:17:30 -0400

    controlflow 01

---
+++ controlflow.py
@@ -0,0 +1,18 @@
+"""hello"""
+# integer variable
+age = 25
+
+# float variables
+temperature = 35.8
+
+# using if/else with integers
+if age >= 25:
+    print("your are an adult")
+else:
+    print("your are an minor")
+
+# using if/else with float variables
+if temperature >= 30.5:
+    print("Temperature is height")
+else:
+    print("temperature is normal")
