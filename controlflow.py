35d272413a4935d60785cb266f0867a2ee86d4a7
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-04-29 20:40:59 -0400

    control flow 02

--- controlflow.py
+++ controlflow.py
@@ -1,18 +1,22 @@
-"""hello"""
+"""Writing program using if/else"""
+def main():
+
 # integer variable
-age = 25
+    age = 25
 
 # float variables
-temperature = 35.8
+    temperature = 35.8
 
 # using if/else with integers
-if age >= 25:
-    print("your are an adult")
-else:
-    print("your are an minor")
+    if age >= 25:
+        print("your are an adult")
+    else:
+        print("your are an minor")
 
 # using if/else with float variables
-if temperature >= 30.5:
-    print("Temperature is height")
-else:
-    print("temperature is normal")
+    if temperature >= 30.5:
+        print("Temperature is height")
+    else:
+        print("temperature is normal")
+if __name__ == "__main__":
+    main()
