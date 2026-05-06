b320f4a6e182f9875e4644526c7a2e23d2b72e0b
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-06 18:09:36 -0400

    Multiplication table using while loop 01

--- while_loop_multiplication.py
+++ while_loop_multiplication.py
@@ -4,8 +4,7 @@ def multiplication_table(num):
     """while loop for multiplication"""
     i = 1
     while i <= 10:
-        print(num, "x", i, "=", num * i);
+        print(num, "x", i, "=", num * i)
         i += 1
 
 multiplication_table(5)
-
