1059eecc4844013565c67bc2a99d5d99e8c85c67
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-05 22:52:25 -0400

    while loop example 03

---
+++ while-loop.py
@@ -0,0 +1,10 @@
+"""Writing sample program on while loop"""
+def count_numbers():
+    """Print numbers from 1 to 5 using while loop"""
+    count = 1
+    while count <= 5:
+        print("count is", count)
+        count += 1
+
+
+count_numbers()
