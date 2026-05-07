3594991f6fe41d5b140138fab2aa0f110e3a0dbf
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-07 19:17:20 -0400

    list expample01

--- list_example.py
+++ list_example.py
@@ -1,8 +1,7 @@
 """Writing a square numbers program using list fuction"""
 
 def square_numbers(numbers):
-    """calculating number with same number"""
-    
+    """calculating number with same number"""    
     return [num ** 2 for num in numbers]
 num = [1, 2, 3, 4]
 result = square_numbers(num)
