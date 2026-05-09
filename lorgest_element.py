8c52b81fed094f506d848e4fe6ed04df21f43e84
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-08 20:47:23 -0400

    Find Lorgest number

---
+++ lorgest_element.py
@@ -0,0 +1,7 @@
+"""Writing this program to print lorgest element in this list"""
+
+def find_lorgest(numbers):
+    return max(nums)
+    
+nums = [10, 15, 9, 18, 13]
+print("Lorgest number", find_lorgest(nums))
