fdb8d159b570ca717bab25eef4004ec439e1a179
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-11 19:02:50 -0400

    Difining variables

---
+++ defining_functions.py
@@ -0,0 +1,40 @@
+"""Writing program for demonstrate defining functions"""
+
+# Simple program to prints message
+def greet():
+    print("Hello welcome to python functions")
+
+# function with parameters
+def add_numbers(a,b):
+    result = a + b
+    return result
+
+# Function with default parameters
+def power(number, exponent=2):
+    return number ** exponent
+
+# Function that performs multi operations
+def calculator(x, y):
+    sum_result = x + y
+    diff_result = x - y
+    prod_result = x * y
+    return sum_result, diff_result, prod_result
+
+# ******************calling the functions ****************
+# call greet function
+greet()
+
+# call add number function
+num1 = 10
+num2 = 5
+sum_value = add_numbers(num1, num2)
+print(f"the sum of {num1} and {num2} is {sum_value}")
+
+# call power function
+print(f"5 squared is {power(5)}")
+print(f"2 rainsed to the power 5 is {power(2, 5)}")
+
+
+# call calculater function
+sum_res, diff_res, prod_res = calculator(8, 3)
+print(f"sum_res: {sum_res}, Difference: {diff_res}, product: {prod_res}")
