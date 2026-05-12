aa04c61cca574f59ef3d70c50b9a0fcbe1b47ffb
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-12 17:06:49 -0400

    Passing arguments

---
+++ arguments.py
@@ -0,0 +1,14 @@
+"""writing python program using functions and arguments"""
+
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+# calling the fuction with arguments
+NUM1 = 10
+NUM2 = 20
+
+SUM = add_numbers(NUM1, NUM2)
+
+# Display the result
+print("The sum =", SUM)
