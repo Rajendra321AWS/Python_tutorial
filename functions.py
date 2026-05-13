a6621a32b9dce9e52c03f6130dd88fa02c56198e
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-13 18:33:48 -0400

    Functions

---
+++ functions.py
@@ -0,0 +1,28 @@
+"""Writing a program using functions"""
+# Function 01
+def great():
+    """Writing Basic fuction """
+    print("Hello Word")
+
+def great_01(NAME):
+    """ Function with arguments"""
+    print("Hello", NAME)
+
+def add(a, b):
+    """Fuction with multi arguments"""
+    result = a + b
+    print("sum of a and b:", result)
+
+def multiply(a, b):
+    """ Function returning value"""
+    return a * b
+    result = multiply(5, 4)
+    print("multiplication", result)
+
+
+
+
+great()
+great_01("Raj")
+add(10, 20)
+multiply(5, 4)
