59b75b72b9afcd6dc3b2ef409ed86bfebe1e4238
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-05-18 09:55:05 -0400

    Simple tuple example

---
+++ tuples_example.py
@@ -0,0 +1,9 @@
+"""Writing Tuples example program."""
+
+def display(items):
+    """Display items in tuple."""
+    print("Apple", "Banana", "Orange")
+
+fruits = ("Apple", "Banana", "Orange")
+
+display(fruits)
