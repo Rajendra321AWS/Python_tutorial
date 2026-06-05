f5cd5a0b2f9c89bd3392cda6f9263e53d941a5b0
rajawsprasad@gmail.com <rajawsprasad@gmail.com>
2026-06-05 18:56:21 -0400

    Regex-Password validation

---
+++ regex_match.py
@@ -0,0 +1,24 @@
+"""Password validation program using regular expressions."""
+
+import re
+
+def check_password(password):
+    """Check whether password is strong."""
+
+    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$'
+
+    if re.match(pattern, password):
+        return "String password"
+    else:
+        return "Week password"
+    
+# Imput from user
+def main():
+    """Read a password and display the validation result"""
+
+    pwd = input("Enter password: ")
+    result = check_password(pwd)
+    print(result)
+
+if __name__=="__main__":
+    main()
